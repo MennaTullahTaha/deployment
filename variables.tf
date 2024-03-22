716fad76006861b5b51f53149e15b1199c37c977
Menna Tullah Magdy Taha <menna.tullah.magdy731@gmail.com>
2024-03-22 13:51:13 +0100

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "gcp_credentials" {
+}
+
+variable "gcp_region" {
+
+}
+
+variable "gke_zone" {
+  type        = string
+  description = "cluster zone"
+}
+
+variable "gcp_project_id" {
+  type        = string
+  description = "GCP project id."
+}
+
+variable "gke_service_account_name" {
+  type        = string
+  description = "Service account name"
+}
